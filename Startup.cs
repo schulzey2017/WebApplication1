64d79bb8948cd7245feed568a52599242dec1f49
schulzey <rikischulz@gmail.com>
2019-10-01 12:25:30 -0500

    stuff2

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace WebApplication1
             //comment
             //stuff
             //stuff2
+            //stuff3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
